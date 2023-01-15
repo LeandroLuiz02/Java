c85b55abf317e3faf0592019ea302b4f353eb09a
Leandro Lima <leandroluiz1906@gmail.com>
2023-01-14 22:01:22 -0300

    interfaces and packeges

---
+++ Pacotes.java
@@ -0,0 +1,7 @@
+import teste.exemplos.escopo2;
+
+public class Pacotes extends escopo2{
+    public static void main(String[] args) {
+        escopo2.printNumber(2);
+    }
+}
---
+++ teste/exemplos/escopo2.java
@@ -0,0 +1,7 @@
+package teste.exemplos;
+
+public class escopo2 {
+    public static void printNumber(int number) {
+        System.out.println(number);
+    }
+}
