a4b7e96a8a14f94d822d27746906207fe0c85f0b
LeandroLuiz02 <leandroluiz1906@gmail.com>
2023-01-19 20:29:02 -0300

    small changes

--- framework.java
+++ framework.java
@@ -9,10 +9,10 @@ public class framework implements Comparable{
     String nome = "";
     public static void main(String[] args) {
 
-        //List<Double> notas = List.of(1d, 4., 5d) ====== igualmente imutável
-//        List<Double> notas = Arrays.asList(1d, 4., 5d); ====== não pode adicionar nem remover itens novos
-//        notas.add(3.);
-//        System.out.println(notas); --------------- DA ERRO -------------------
+//          List<Double> notas = List.of(1d, 4., 5d) ====== igualmente imutável
+//          List<Double> notas = Arrays.asList(1d, 4., 5d); ====== não pode adicionar nem remover itens novos
+//          notas.add(3.);
+//          System.out.println(notas); --------------- DA ERRO -------------------
 
         List<Integer> n = new ArrayList<>();
         n.add(1);
